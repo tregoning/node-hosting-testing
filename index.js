89ede43068093718c0fe8ee8d8ce770c1f089588
John Tregoning <tregoning@users.noreply.github.com>
2018-09-25 10:57:47 -0700

    1st commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
+express()
+	.get('/', (req, res) => res.send('Hello World!'))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
